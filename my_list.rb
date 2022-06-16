28e07a1e06cf855cd6a2b5ad3090b72af34c3c90
sainingo <nickmentor421@gmail.com>
2022-06-16 23:05:07 +0300

    fix linter

---
+++ my_list.rb
@@ -0,0 +1,17 @@
+# $LOAD_PATH <<'.'
+require_relative 'MyEnumerable'
+class MyList
+  include MyEnumerable
+
+  def initialize(*list)
+    @list = list
+  end
+
+  def each(&block)
+    @list.each(&block)
+  end
+end
+
+l = MyList.new(2, 3, 4)
+puts l.inspect
+puts(l.all? { |e| e < 5 })
